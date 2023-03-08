ae279ff82b757c6c3a9f0f7dd438cf0535f075ac
Émile <emile.royer@epfedu.fr>
2023-03-08 12:28:49 +0100

    Take draws into account
    And new dataset

--- niveau-bayésien.cs
+++ niveau-bayésien.cs
@@ -17,8 +17,9 @@ namespace myApp
 			//     Création des données
 			// *********************************
 			// The winner and loser in each of 6 samples games
-			var winnerData = new[] { 0, 0, 0, 1, 3, 4 };
-			var loserData = new[] { 1, 3, 4, 2, 1, 2 };
+			var winnerData = new[] { 0, 2, 2, 1, 2, 0, 5, 4, 4, 6, 7, 6 };
+			var loserData = new[] { 1, 3, 0, 3, 1, 3, 6, 7, 5, 4, 5, 7 };
+			var drawData = new[] { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 };
 
 			// Define the statistical model as a probabilistic program
 			var game = new Range(winnerData.Length);
@@ -28,6 +29,7 @@ namespace myApp
 
 			var winners = Variable.Array<int>(game);
 			var losers = Variable.Array<int>(game);
+			var draws = Variable.Array<int>(game);
 
 			// *******************************************
 			//    Déclaration de la méthode d'inférence
@@ -35,16 +37,35 @@ namespace myApp
 			using (Variable.ForEach(game))
 			{
 				// The player performance is a noisy version of their skill
-				var winnerPerformance = Variable.GaussianFromMeanAndVariance(playerSkills[winners[game]], 1.0);
-				var loserPerformance = Variable.GaussianFromMeanAndVariance(playerSkills[losers[game]], 1.0);
+				Variable<double> winnerPerformance = Variable.GaussianFromMeanAndVariance(playerSkills[winners[game]], 1.0);
+				Variable<double> loserPerformance = Variable.GaussianFromMeanAndVariance(playerSkills[losers[game]], 1.0);
+				Variable<int> draw = draws[game];
+				var drawMargin = Variable.New<double>().Named("drawMargin");
+				var drawMarginPrior = Variable.New<Gaussian>().Named("drawMarginPrior");
+				drawMarginPrior.SetTo(new Gaussian(1, 10));
+				drawMargin.SetTo(Variable<double>.Random(drawMarginPrior));
+				Variable.ConstrainTrue(drawMargin > 0);
 
-				// The winner performed better in this game
-				Variable.ConstrainTrue(winnerPerformance > loserPerformance);
+				var diff = (winnerPerformance - loserPerformance).Named("diff");
+
+				using (Variable.Case(draw, 0))
+				{
+					// The winner performed better in this game
+					Variable.ConstrainTrue(winnerPerformance > loserPerformance);
+				}
+
+				using (Variable.Case(draw, 1))
+				{
+					// In a case of a draw, the difference is lower than the margin
+					Variable.ConstrainBetween(diff, -drawMargin, drawMargin);
+				}
 			}
 
 			// Attach the data to the model
 			winners.ObservedValue = winnerData;
 			losers.ObservedValue = loserData;
+			draws.ObservedValue = drawData;
+
 
 			// **********************************
 			//    Effectuation de l'inférence
